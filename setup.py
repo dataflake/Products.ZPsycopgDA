98fa7aaa2775fc5f9b9b60a66457145fa89e32ff
Jens Vagelpohl <jens@plyp.com>
2025-10-30 16:14:16 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ long_description = (
 
 setup(
     name='Products.ZPsycopgDA',
-    version='4.4',
+    version='4.5.dev0',
     license='ZPL-2.1',
     license_files=['LICENSE*'],
     author='Federico Di Gregorio',
